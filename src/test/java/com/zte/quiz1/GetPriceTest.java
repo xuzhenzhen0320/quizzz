c2bfe67972a3868a1cf3ad37e5d67762e5677462
e <545209138@qq.com>
2018-01-13 22:29:15 +0800

    add Test

--- src/test/java/com/zte/quiz1/GetPriceTest.java
+++ src/test/java/com/zte/quiz1/GetPriceTest.java
@@ -2,7 +2,9 @@ package com.zte.quiz1;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -10,44 +12,24 @@ public class GetPriceTest {
     @Test
     public void getPrice() throws Exception {
 
-       /* double normalPrice1 = 10;
-        HashMap<String,Integer> map1 = new HashMap<String, Integer>();
-        map1.put("新书",1);
-        map1.put("常规图书",1);
-        map1.put("滞销图书",1);
-        assertEquals(28,new GetPrice().getPrice(map1,normalPrice1),0);
+        List<Double> newPrice1 = new ArrayList<Double>();
+        List<Double> normalPrice1 = new ArrayList<Double>();
+        List<Double> oldPrice1 = new ArrayList<Double>();
+        newPrice1.add(10.0);
+        normalPrice1.add(10.0);
+        oldPrice1.add(10.0);
+        Book book1 = new Book("新书",1,newPrice1);
+        Book book2 = new Book("常规图书",1,normalPrice1);
+        Book book3 = new Book("滞销图书",1,oldPrice1);
 
-        double normalPrice2 = 8.8;
-        HashMap<String,Integer> map2 = new HashMap<String, Integer>();
-        map2.put("新书",56);
-        map2.put("常规图书",100);
-        map2.put("滞销图书",60);
-        assertEquals(1788.1600000000003,new GetPrice().getPrice(map2,normalPrice2),0);
 
-        double normalPrice3 = 5;
-        HashMap<String,Integer> map3 = new HashMap<String, Integer>();
-        map3.put("",1);
-        map3.put("哈哈",1);
-        map3.put("滞销图书",1);
-        assertEquals(-1,new GetPrice().getPrice(map3,normalPrice3),0);
+        List<Book> books1 = new ArrayList<Book>();
+        books1.add(book1);
+        books1.add(book2);
+        books1.add(book3);
+        assertEquals(28,new GetPrice().getPrice(books1),0);
 
-        double normalPrice4 = -10;
-        HashMap<String,Integer> map4 = new HashMap<String, Integer>();
-        map4.put("新书",1);
-        map4.put("常规图书",1);
-        map4.put("滞销图书",1);
-        assertEquals(-1,new GetPrice().getPrice(map4,normalPrice4),0);
 
-        double normalPrice5 = 10;
-        HashMap<String,Integer> map5 = new HashMap<String, Integer>();
-        map5.put("新书",-20);
-        map5.put("常规图书",1);
-        map5.put("滞销图书",1);
-        assertEquals(-1,new GetPrice().getPrice(map5,normalPrice5),0);
-
-        double normalPrice6 = 10;
-        HashMap<String,Integer> map6 = new HashMap<String, Integer>();
-        assertEquals(-1,new GetPrice().getPrice(map6,normalPrice6),0);*/
 
     }
 
