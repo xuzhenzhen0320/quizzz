6f0f74c8d51d45d6a832781c04f01697e8bba8c2
e <545209138@qq.com>
2018-01-14 17:11:09 +0800

    delete Map

--- src/main/java/com/zte/quiz1/Main.java
+++ src/main/java/com/zte/quiz1/Main.java
@@ -1,7 +1,6 @@
 package com.zte.quiz1;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
--- src/test/java/com/zte/quiz1/GetPriceTest.java
+++ src/test/java/com/zte/quiz1/GetPriceTest.java
@@ -3,7 +3,6 @@ package com.zte.quiz1;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.Assert.*;
