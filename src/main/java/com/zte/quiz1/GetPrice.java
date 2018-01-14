9f1ac577f67d26c64bbbde19dc9486ea3aedd058
e <545209138@qq.com>
2018-01-14 16:40:49 +0800

    add judge

--- src/main/java/com/zte/quiz1/GetPrice.java
+++ src/main/java/com/zte/quiz1/GetPrice.java
@@ -3,13 +3,16 @@ package com.zte.quiz1;
 import java.util.List;
 
 /**
- * 输入：书的种类、数量和原始价格
+ * 输入：书的种类、数量和价格
  * 输出：价格
- * 例如：输入：新书 3 常规图书 2 滞销图书 5
+ * 例如：输入：新书 2
+ *            10 20
+ *           常规图书 1
+ *           10
+ *           滞销图书 3
+ *           20 10 3
  *
- *            10
- *
- *      输出：86
+ *      输出：65.8
  *
  */
 public class GetPrice
@@ -17,34 +20,52 @@ public class GetPrice
 
     public double getPrice(List<Book> books){
         double price = 0;
-        for (int i = 0;i < books.size();i++) {
-            if(books.get(i).getType().equals("新书")){
-                double newPrice = 0;
-                for(int j = 0;j < books.get(i).getCount();j++){
-                    newPrice += books.get(i).getPriceList().get(j);
-                }
-                price += newPrice*1.2;
+        //判断输入的合法性
 
-            }
-            else if(books.get(i).getType().equals("常规图书")){
-                double normalPrice = 0;
-                for(int j = 0;j < books.get(i).getCount();j++){
-
-                    normalPrice += books.get(i).getPriceList().get(j);
-                }
-                price += normalPrice * 1.0;
-            }
-            else if(books.get(i).getType().equals("滞销图书")){
-                double oldPrice = 0;
-                for(int j = 0;j < books.get(i).getCount();j++){
-                    oldPrice += books.get(i).getPriceList().get(j);
-                }
-                price += oldPrice * 0.6;
-            }
-            else{
+        for (Book book:books){
+            if(!book.getType().equals("新书") && !book.getType().equals("常规图书") && !book.getType().equals("滞销图书")){
                 System.out.println("您输入的图书类型不合法。");
+                return -1;
+            }
+            if(book.getCount() < 0){
+                System.out.println("您输入的图书数量不合法。");
+                return -1;
+            }
+            if(book.getCount() != book.getPriceList().size()){
+                System.out.println("您输入的图书数量与价格的数量不一致。");
+                return -1;
             }
 
+            for(int i = 0;i < book.count;i++){
+                if(book.getPriceList().get(i) < 0){
+                    System.out.println("您输入的书的价格不合法");
+                    return -1;
+                }
+            }
+
+        }
+        //输入合法则开始计算
+        for (Book book:books) {
+                if (book.getType().equals("新书")) {
+                    double newPrice = 0;
+                    for (int i = 0; i < book.getCount(); i++) {
+                        newPrice += book.getPriceList().get(i);
+                    }
+                    price += newPrice * 1.2;
+
+                } else if (book.getType().equals("常规图书")) {
+                    double normalPrice = 0;
+                    for (int i = 0; i < book.getCount(); i++) {
+                        normalPrice += book.getPriceList().get(i);
+                    }
+                    price += normalPrice * 1.0;
+                } else if (book.getType().equals("滞销图书")) {
+                    double oldPrice = 0;
+                    for (int i = 0; i < book.getCount(); i++) {
+                        oldPrice += book.getPriceList().get(i);
+                    }
+                    price += oldPrice * 0.6;
+                }
         }
         return price;
     }
--- src/main/java/com/zte/quiz1/Main.java
+++ src/main/java/com/zte/quiz1/Main.java
@@ -6,13 +6,16 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * 输入：书的种类、数量和原始价格
+ * 输入：书的种类、数量和价格
  * 输出：价格
- * 例如：输入：新书 3 常规图书 2 滞销图书 5
+ * 例如：输入：新书 2
+ *            10 20
+ *           常规图书 1
+ *           10
+ *           滞销图书 3
+ *           20 10 3
  *
- *            10
- *
- *      输出：86
+ *      输出：65.8
  *
  */
 public class Main {
@@ -28,7 +31,7 @@ public class Main {
         System.out.println("请输入该类型图书每本书的价格，用空格隔开");
         List<Double> priceList1 = new ArrayList<Double>();
         String ss1[] = sc.nextLine().split(" ");
-        for(int i = 0;i < count1;i++){
+        for(int i = 0;i < ss1.length;i++){
             priceList1.add(Double.parseDouble(ss1[i]));
         }
         Book book1 = new Book(type1,count1,priceList1);
@@ -42,7 +45,7 @@ public class Main {
         System.out.println("请输入该类型图书每本书的价格，用空格隔开");
         List<Double> priceList2 = new ArrayList<Double>();
         String ss2[] = sc.nextLine().split(" ");
-        for(int i = 0;i < count2;i++){
+        for(int i = 0;i < ss2.length;i++){
             priceList2.add(Double.parseDouble(ss2[i]));
         }
         Book book2 = new Book(type2,count2,priceList2);
@@ -56,7 +59,7 @@ public class Main {
         System.out.println("请输入该类型图书每本书的价格，用空格隔开");
         List<Double> priceList3 = new ArrayList<Double>();
         String ss3[] = sc.nextLine().split(" ");
-        for(int i = 0;i < count3;i++){
+        for(int i = 0;i < ss3.length;i++){
             priceList3.add(Double.parseDouble(ss3[i]));
         }
         Book book3 = new Book(type3,count3,priceList3);
