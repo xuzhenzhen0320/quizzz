97bb364f2d122e7e7fd6865a44c4afdc7d01bcd6
e <545209138@qq.com>
2018-01-14 16:53:24 +0800

    add test2

--- src/test/java/com/zte/quiz1/GetPriceTest.java
+++ src/test/java/com/zte/quiz1/GetPriceTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 public class GetPriceTest {
     @Test
     public void getPrice() throws Exception {
-
+        //测试用例1
         List<Double> newPrice1 = new ArrayList<Double>();
         List<Double> normalPrice1 = new ArrayList<Double>();
         List<Double> oldPrice1 = new ArrayList<Double>();
@@ -28,6 +28,26 @@ public class GetPriceTest {
         books1.add(book2);
         books1.add(book3);
         assertEquals(28,new GetPrice().getPrice(books1),0);
+
+        //测试用例2
+        List<Double> newPrice2 = new ArrayList<Double>();
+        List<Double> normalPrice2 = new ArrayList<Double>();
+        List<Double> oldPrice2 = new ArrayList<Double>();
+        newPrice2.add(10.0);
+        normalPrice2.add(10.0);
+        oldPrice2.add(10.0);
+        Book book4 = new Book("",1,newPrice2);
+        Book book5 = new Book("haha",1,normalPrice2);
+        Book book6 = new Book("滞销图书",1,oldPrice2);
+
+
+        List<Book> books2 = new ArrayList<Book>();
+        books2.add(book4);
+        books2.add(book5);
+        books2.add(book6);
+        assertEquals(-1,new GetPrice().getPrice(books2),0);
+
+
 
 
 
